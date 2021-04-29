5de5e66fd943a15f36b3401a24fb4d1d18369a91
Sergio <sergio.hernandez@littlestarmedia.com>
2021-04-29 08:32:00 +0100

    commint advance

--- src/Congrats.spec.js
+++ src/Congrats.spec.js
@@ -1,17 +1,18 @@
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import { findByTestAttr } from './test/testUtils';
+import { findByTestAttr, checkProps } from './test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 const setup = (props={}) => {
+  // const setupProps = { ...defaultProps };
   return shallow(<Congrats {...props}/>);
 }
 
 it('render without error', () => {
-  const wrapper = setup();
+  const wrapper = setup({success: true});
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
@@ -27,3 +28,9 @@ it('render no-empty congrats message when success prop is true', () => {
   const component = findByTestAttr(wrapper, 'congrats-message');
   expect(component.text().length).not.toBe(0);
 });
+
+it('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
+});
+
